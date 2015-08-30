07542b1d388e7f67e25fb7b12437a680cdbabde0
Ariel Spear <simpleredfox@gmail.com>
2015-08-30 06:00:17 -0700

    updating db file

--- src/test/java/DataBaseRule.java
+++ src/test/java/DataBaseRule.java
@@ -4,15 +4,15 @@ import org.sql2o.*;
 public class DataBaseRule extends ExternalResource {
 
   protected void before() {
-    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/TESTDBNAME", "jake", "password");
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon_test", null, null);
    }
 
   protected void after() {
     try(Connection con = DB.sql2o.open()) {
-      String deleteTasksQuery = "DELETE FROM tasks *;";
-      String deleteCategoriesQuery = "DELETE FROM categories *;";
-      con.createQuery(deleteTasksQuery).executeUpdate();
-      con.createQuery(deleteCategoriesQuery).executeUpdate();
+      String deleteClientsQuery = "DELETE FROM clients *;";
+      String deleteStylistsQuery = "DELETE FROM stylists *;";
+      con.createQuery(deleteClientsQuery).executeUpdate();
+      con.createQuery(deleteStylistsQuery).executeUpdate();
     }
   }
 }
